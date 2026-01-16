e881906fe57bef1f592765d9e3730078317985a6
Atharva <atharvabhise1@gmail.com>
2026-01-16 09:42:25 +0530

    SG change

--- wordpress-terraform/modules/networking/main.tf
+++ wordpress-terraform/modules/networking/main.tf
@@ -86,7 +86,7 @@ resource "aws_security_group" "web_srv_sg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = [ "0.0.0.0/0" ]
+    security_groups = [ aws_security_group.alb_sg.id ]
   }
   ingress {
     from_port = 22
