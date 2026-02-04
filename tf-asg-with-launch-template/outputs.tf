c31a3e8fa33ea871ac529051b098934f7e2ad702
Atharva <atharvabhise1@gmail.com>
2026-02-04 09:07:54 +0530

    added outputs

--- tf-asg-with-launch-template/outputs.tf
+++ tf-asg-with-launch-template/outputs.tf
@@ -1,3 +1,11 @@
 output "pub_sub_ids" {
   value = module.vpc.pub_sub_ids
 }
+
+output "asg_name" {
+  value = module.asg.asg_name
+}
+
+output "launch_template_id" {
+  value = module.asg.launch_template_id
+}
